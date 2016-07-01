1cf5f53443c1cd3624d08bf813bb7da0cde9af74
Gary Deer <gdeer81@gmail.com>
2016-07-01 18:17:44 -0500

    more initial project setup

---
+++ src/om_commerce/core.cljs
@@ -0,0 +1,9 @@
+(ns om-commerce.core
+  (:require [goog.dom :as gdom]
+            [om.next :as om :refer-macros [defui]]
+            [om.dom :as dom]))
+
+(enable-console-print!)
+
+(println "All Systems go!")
+
