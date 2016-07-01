aeb2031387a910c6ac3e65ed912ca2387ef1ecf5
Gary Deer <gdeer81@gmail.com>
2016-07-01 17:48:48 -0500

    project setup

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject om-commerce "0.1.0-SNAPSHOT"
+  :description "An Ecommerce Site writeen in Om!"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [org.omcljs/om "1.0.0-alpha34"]
+                 [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "test"]])
